967d4f5fd79d55b27f6f3a4ccc51fa87956c98bc
Lucas Diedrich <diedrich@biochem.mpg.de>
2026-06-11 09:49:45 +0200

    [Dep] Enforce dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='scprotvelo',
+    name="scprotvelo",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "scvi-tools==1.1.5",
+        "scvelo==0.3.2",
+        "scanpy==1.9.1",
+        "anndata==0.10.7",
+        "matplotlib==3.6.3",
+        "pandas==1.5.3",
+        "jupyter==1.1.1",
+        "matplotlib-inline==0.1.7"
+    ],
 )
